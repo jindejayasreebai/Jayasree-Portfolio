5c2a514a9f8349e927e6570a1e5b1f8d53a6c00b
Jinde Jayasree Bai <141146177+jindejayasreebai@users.noreply.github.com>
2024-03-07 15:28:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,148 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  window.onscroll = function() {headerShadow()};
+
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+
+    } else {
+
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+
+    }
+  }
+
+
+/* ----- TYPING EFFECT ----- */
+ var typingEffect = new Typed(".typedText",{
+    strings : ["Web Developer","BackEnd Developer","FrontEnd Developer"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+    }  else {
+
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Select the form element
+  const contactForm = document.getElementById("contact form");
+
+  // Add event listener for form submission
+  contactForm.addEventListener("submit", function(event) {
+      // Prevent the default form submission
+      event.preventDefault();
+
+      // Collect form data
+      const formData = new FormData(contactForm);
+
+      // Send form data to the server using AJAX
+      fetch("https://script.google.com/macros/s/AKfycbw0vMjWUJou_N-BPnuExwOey6NKA6MiW68qneO8c-TZu5egAIg8biE54Ya-6GaSRHe5/exec", {
+          method: "POST",
+          body: formData
+      })
+      .then(response => response.json()) // Assuming the server returns JSON
+      .then(data => {
+          // Display response message
+          document.getElementById("response-message").textContent = data.message;
+      })
+      .catch(error => {
+          console.error("Error:", error);
+          // Display error message if something goes wrong
+          document.getElementById("response-message").textContent = "An error occurred. Please try again later.";
+      });
+  });
+});
+
